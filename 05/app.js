958e72da2152b3ab0ae6d504329f73f5ff95c11c
Daria Seńko <twoj@email>
2025-07-21 17:53:44 +0200

    05

--- 05/app.js
+++ 05/app.js
@@ -1,16 +1,27 @@
+import Junior from './classes/Junior.js';
+import Mid from './classes/Mid.js';
+import Senior from './classes/Senior.js';
 import Programmer from './classes/Programmer';
 
 const features = {
-    skills: 10, 
-    experience: 10, 
-    willingness: 10,
-}
+  skills: 10,
+  experience: 10,
+  willingness: 10,
+};
 
 const task = {
-    difficult: 1,
-    size: 1,
-}
+  difficult: 1,
+  size: 1,
+};
 
-const programmer = new Programmer( features );
-const time = programmer.getApproximateTimeTaskDoneInHours( task );
+const junior = new Junior();
+const mid = new Mid();
+const senior = new Senior();
+
+console.log('Junior time:', junior.getApproximateTimeTaskDoneInHours(task));
+console.log('Mid time:', mid.getApproximateTimeTaskDoneInHours(task));
+console.log('Senior time:', senior.getApproximateTimeTaskDoneInHours(task));
+
+const programmer = new Programmer(features);
+const time = programmer.getApproximateTimeTaskDoneInHours(task);
 console.log(time);
